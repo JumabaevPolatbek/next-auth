b9c267710497cc6bc1d55d8a0efcac28d45bd145
JumabaevPolatbek <112640866+JumabaevPolatbek@users.noreply.github.com>
2023-05-11 19:36:36 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains:['**']
-// 		remotePatterns: [
-// 			{
-// 				protocol: 'https',
-// 				hostname: '**',
-// 			},
-// 		],
+// 		domains:['**']
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**',
+			},
+		],
 // 		formats: ['image/webp', 'image/avif'],
 	},
 };
