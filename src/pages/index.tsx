589cdb2eca24460ba8fad73171c4c4bd52518db8
JumabaevPolatbek <mouz.cyx@mail.ru>
2023-05-13 19:21:23 +0500

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,11 @@ import { useSession } from "next-auth/react";
 function Home() {
   const { data, status } = useSession();
   if (status === "authenticated") {
-    return;
+    return (
+      <div>
+        <h2>Welcom {data?.user.decode.sub}</h2>
+      </div>
+    );
   }
   return (
     <>
